11591c2996b9d3645d69731a6cc8c35ac0b82ffd
velesov7493 <velesov7493@yandex.ru>
2021-06-26 16:58:06 +0000

    Тест для codecov.io

---
+++ src/test/java/ru/job4j/grabber/AppSettingsTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.grabber;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class AppSettingsTest {
+
+    @Test
+    public void whenLoadProperties() {
+        Properties ps = AppSettings.loadProperties();
+        assertEquals("5", ps.getProperty("rabbit.interval"));
+        assertEquals("org.postgresql.Driver", ps.getProperty("jdbc.driver"));
+        assertEquals("jdbc:postgresql://localhost:5432/grabber_db", ps.getProperty("jdbc.url"));
+        assertEquals("root", ps.getProperty("jdbc.username"));
+        assertEquals("qx8eec", ps.getProperty("jdbc.password"));
+    }
+}
