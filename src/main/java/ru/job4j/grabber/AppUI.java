802813cd736ee6f72e90a008338aa0c900b19b33
velesov7493 <velesov7493@yandex.ru>
2021-06-27 07:31:23 +0000

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/AppUI.java
+++ src/main/java/ru/job4j/grabber/AppUI.java
@@ -5,6 +5,9 @@ import ru.job4j.grabber.html.SqlRuParser;
 public class AppUI {
 
     public static void main(String[] args) {
-        SqlRuParser.parse("https://www.sql.ru/forum/job-offers");
+        for (int i = 1; i <= 5; i++) {
+            String page = i == 1 ? "" : "/" + i;
+            SqlRuParser.parse("https://www.sql.ru/forum/job-offers" + page);
+        }
     }
 }
