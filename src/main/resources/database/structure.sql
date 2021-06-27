1990206547bfb51b8d0ec7263d07cc7afb7da3e3
velesov7493 <velesov7493@yandex.ru>
2021-06-27 11:16:43 +0000

    4. Схема таблицы Post [#1731]

--- src/main/resources/database/structure.sql
+++ src/main/resources/database/structure.sql
@@ -1,6 +1,16 @@
 DROP TABLE IF EXISTS tz_rabbits;
+DROP TABLE IF EXISTS tz_posts;
 
 CREATE TABLE tz_rabbits (
     id SERIAL PRIMARY KEY,
     created_date TIMESTAMP
+);
+
+CREATE TABLE tz_posts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL UNIQUE,
+    authorName VARCHAR(90),
+    text TEXT,
+    created TIMESTAMP NOT NULL
 );
